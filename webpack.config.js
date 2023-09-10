2da8da6acc22f90af4f06bb07b081c7a12cfd3be
daniel-kav <dankavatha01@gmail.com>
2023-09-10 14:59:14 +0300

    refactored css loaders

--- webpack.config.js
+++ webpack.config.js
@@ -15,17 +15,9 @@ module.exports = {
                 use: ['style-loader', 'css-loader'], // Loaders to process CSS files
             },
             {
-                test: /\.(png|jpg|jpeg|gif|svg)$/i, // Match image file extensions
-                use: [
-                    {
-                        loader: 'url-loader',
-                        options: {
-                            limit: 8192, // Convert images <= 8kb to base64 strings (adjust the limit as needed)
-                            name: 'images/[name].[ext]', // Output file path and name
-                        },
-                    },
-                ],
-            },
+                test: /\.(png|svg|jpg|gif|jpeg)$/i,
+                type: 'asset/resource'
+              },
         ],
     },
 };
